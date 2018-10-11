c1c788afcc9e1142c90fdc9a2085f0d36e5d2543
Katie O Donnell <katiemodonnell88@gmail.com>
2018-10-11 15:04:35 +0000

    Can add questions

--- quiz.py
+++ quiz.py
@@ -1,4 +1,5 @@
 def show_menu():
+    print("---------")
     print("Quiz Game")
     print("---------")
     print()
@@ -10,10 +11,25 @@ def show_menu():
     option = input("Enter option: ")
     return option
 
-
+def add_a_question():
+    question = input("Enter a question: ")
+    answer = input("ok then tell me, " + question + ": ")
+    
+    with open("questions.txt", "a") as f:
+        line = question + "|" + answer + "\n"
+        f.write(line)
+    
 
 while True: 
     option = show_menu()
     
+    if option == "1":
+        print("you picked run quiz")
+    
+        
+    if option == "2":
+       add_a_question()
+        
+        
     if option == "3":
         break
